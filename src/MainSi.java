f13bbb925eb132797fc3b1cb8286dc13e3a3d74b
Ecomont <hector19.rl@gmail.com>
2023-03-08 16:56:33 +0100

    jn

--- src/MainSi.java
+++ src/MainSi.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class MainSi {
+import javax.swing.*;
+
+public class MainSi {
+    private JLabel usernameLabel;
+    private JTextField usernameTextField;
+    private JLabel passwordLabel;
+    private JTextField passwordTextField;
+    private JPanel mainPanel;
+
+    public MainSi(){
+
+    }
 }
